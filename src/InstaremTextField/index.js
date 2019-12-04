ecfadacf32ed4e48081b8253b716bf2f43d319af
Sujit <pushpak.negobot@perennialsys.com>
2019-12-04 17:27:05 +0530

    StreamInputField_updation : Sujit.Kaulavkar : Added changes for InstaremTextField.

--- src/InstaremTextField/index.js
+++ src/InstaremTextField/index.js
@@ -100,7 +100,7 @@ class InstaremTextField extends Component {
     super(props);
     this.state = {
       autoFocusEnabled: this.props.autoFocus || false,
-      shouldEncryptedTextBeVisible: false,
+      shouldEncryptedTextBeVisible: this.props.isSecureText || false,
       showPlaceHolderForFloating: false
     };
   }
@@ -123,9 +123,11 @@ class InstaremTextField extends Component {
       textStyle,
       labelPadding,
       labelHeight,
-      inputContainerPadding
+      inputContainerPadding,
+      input: {value: preProcessedValue},
     } = this.props;
 
+    const value = preProcessedValue && preProcessedValue.toString();
     let isTintedUnderlineRequired =
       underlineType === TEXTFIELD_UNDERLINESTYLE.textMatch &&
       this.props.shape === TEXTFIELD_SHAPES.rectangular;
@@ -165,9 +167,11 @@ class InstaremTextField extends Component {
               placeholderTextColor={!isFloating ? placeholderColor || '' : null}
               underlineColorAndroid='transparent'
               multiline={multiline}
+              secureTextEntry={this.state.shouldEncryptedTextBeVisible}
               numberOfLines={multiline ? numberOfLines : 1}
               returnKeyType={returnKeyType}
               onChangeText={this.onTextChanged.bind(this)}
+              value={value}
               onFocus={this.onFocus.bind(this)}
               onBlur={this.onBlur.bind(this)}
               responsive={false}
@@ -276,11 +280,18 @@ class InstaremTextField extends Component {
   }
 
   getAccessoryView = () => {
-    if (this.props.showClearButton) {
+
+    if (this.props.showClearButton && this.props.showShowHideButton) {
+      return <View style={{flexDirection: 'row'}}>
+        {this.getRightSideShowButton()}
+        {this.getRightSideClearButton()}        
+      </View>
+    } else if (this.props.showClearButton) {
       return this.getRightSideClearButton();
     } else if (this.props.showShowHideButton) {
       return this.getRightSideShowButton();
-    } else {
+    }     
+    else {
       return null;
     }
   };
@@ -292,7 +303,7 @@ class InstaremTextField extends Component {
       let clearButtonSource = this.props.clearButtonImage || cancelIcon;
 
       return (
-        <TouchableOpacity onPress={this.clearText}>
+        <TouchableOpacity style={[styles.rightAccessory, this.props.rightAccessoryStyle]} onPress={this.clearText}>
           <Image
             style={[styles.image, this.props.clearButtonStyle]}
             source={clearButtonSource}
@@ -313,7 +324,7 @@ class InstaremTextField extends Component {
       let showButtonSource = this.props.showButtonImage || showButton;
 
       return (
-        <TouchableOpacity onPress={this.showHideSecureText}>
+        <TouchableOpacity style={[styles.rightAccessory, this.props.rightAccessoryStyle]} onPress={this.showHideSecureText}>
           <Image
             style={[styles.image, this.props.clearButtonStyle]}
             source={
@@ -345,17 +356,21 @@ class InstaremTextField extends Component {
   };
 
   onTextChanged(text) {
+    const {input: {onChange}} = this.props;
     let refinedText = this.handleChange(text);
+    onChange(text);
     if (this.props.onChangeText) {
       this.props.onChangeText(refinedText);
     }
   }
 
   clearText = () => {
-    this.inputRef.current.clear();
-    if (this.props.onChangeText) {
-      this.props.onChangeText('');
-    }
+    if (this.props.showClearButton !== 1) { // If the entered text is valid then avoid the text clear action. 
+      this.inputRef.current.clear();
+      if (this.props.onChangeText) {
+        this.props.onChangeText('');
+      }
+    }        
   };
 
   focus = () => {
@@ -424,7 +439,13 @@ const styles = StyleSheet.create({
     flex: 0.01
   },
   image: {
-    margin: 10
+    marginTop: 0,
+  },
+  rightAccessory: {
+    width: 40,
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
 
